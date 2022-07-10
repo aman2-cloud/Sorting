b0bc4bfd389993adbe940da60575755aaa909b5e
AMAN VYAS <amanvyas055@gmail.com>
2022-07-10 21:26:30 +0530

    Initial commit

---
+++ src/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={7,4,5,6,3,10};
+        for(int i=0;i<arr.length-1;i++){
+            int smallest=i;
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            }
+            //swap
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
+        }
+        printArray(arr);
+    }
+}
